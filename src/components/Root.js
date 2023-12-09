681ae41e8505d45872c8328b174679ccefe35d10
KalimGithub <sk22kalim@gmail.com>
2023-12-09 22:32:25 +0530

    assignment done

---
+++ src/components/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Root = () => {
+
+    return (
+        <div id='root'>
+            <p>I am learning React. My life is getting better.</p>
+        </div>
+    )
+}
+
+export default Root;
